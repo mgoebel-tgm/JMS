d6cd5d5a3b643c60990827739edb01934e090ba7
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-11-20 08:11:21 +0100

    Überprüfung von ip und username beim Starten

--- Start/CheckArguments.java
+++ Start/CheckArguments.java
@@ -7,6 +7,18 @@ package Start;
 public class CheckArguments {
 	
 	/**
+	 * Ueberprueft ob es ein "gueltiger" Username ist (3-15 Zeichen ( a-z, 0-9, _ , -)
+	 * @param name der ueberpruefende Name
+	 * @return ob es gueltig ist
+	 */
+	public static boolean checkUsername(String name){
+		String USERNAME_PATTERN= "^[a-z0-9_-]{3,15}$";
+		if(name.matches(USERNAME_PATTERN))
+			return true;
+		System.err.println("Not a valid username, it need to have 3-15 characters (include numbers, underline, minus)");
+		return false;
+	}
+	/**
 	 * Ueberprueft ob es eine gueltige IP-Adresse ist (vom Format her)
 	 * @param address die zu pruefende adresse
 	 * @return ob es gueltig ist
@@ -15,6 +27,9 @@ public class CheckArguments {
 		String IPADDRESS_PATTERN = "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
 		if ((address.matches(IPADDRESS_PATTERN)))
 			return true;
+		System.err.println("Not a valid IP-Address.");
 		return false;
 	}
+	
+	
 }
--- Start/StartChat.java
+++ Start/StartChat.java
@@ -2,32 +2,59 @@ package Start;
 
 import java.io.*;
 
+import org.apache.activemq.*;
+
+import javax.jms.*;
+
 /**
  * Starten eines Chats und das Parameterhandling
  * @author Melanie Goebel
  * @version 2014-11-14
  */
 public class StartChat {
+	private static String ip;
+	private static String username;
+	
 	public static void main (String[] args){
-		if(args.length == 1 && CheckArguments.checkIP(args[0])==true){
+		
 			InputStreamReader in = new InputStreamReader(System.in);
 			BufferedReader r = new BufferedReader(in);
 			String input = "";
+<<<<<<< HEAD
+			try {
+				boolean validIP = false;
+				boolean validName = false;
+				while(validIP == false || validName == false){
+					System.out.print("IP Address: ");
+					ip = r.readLine();
+					validIP = CheckArguments.checkIP(ip);
+					System.out.print("Username: ");
+					username = r.readLine();
+					validName = CheckArguments.checkUsername(username);
+				}
+				
+			} catch (IOException e) {
+				System.err.println("Something is wrong with input/output while giving username and ip-address");
+			}
+			connect(ip,username);
+			System.out.println("Hello "+username+"! \n if you need help enter help");
+=======
 			String[] inputSplit;
 			System.out.println("Hello! \n if you need help enter --help");
+>>>>>>> 2af12cd85e68779a7544bb7a8f149fc281432531
 			while(true){
 				try {
 					input = r.readLine();
 				} catch (IOException e1) {
-					System.out.println("Error: IO");
+					System.out.println("Something is wrong with input/output !");
 				}
 				inputSplit = input.split(" ");
 				switch(inputSplit[0]){
 				case "help":
 					System.out.println("chat <ip-address> <username> <chatroom> \n"
-									 + "mail <ip-address> <nachricht> \n"
-									 + "mailbox"
-									 + "exit");
+							+ "mail <ip-address> <nachricht> \n"
+							+ "mailbox"
+							+ "exit");
 					break;
 
 				case "mail":
@@ -41,14 +68,24 @@ public class StartChat {
 				case "mailbox":
 					System.out.println("No mailbox");
 					//TODO:Tobi Mailbox implementieren
-				    break;
+					break;
 				case "exit":
 					System.out.println("Bye!");
 					System.exit(0);
 				}
 			}
-		}else{
-			System.err.println("Gueltige IP-Adresse bitte mit eingeben!!!");
-		}
 	}
+	public static Connection connect(String user, String ip){
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(user, ActiveMQConnection.DEFAULT_PASSWORD,"failover://tcp://" + ip + ":61616");
+		Connection connection = null;
+		//create Connection
+		try {
+			connection = connectionFactory.createConnection();
+			connection.start();
+		} catch (JMSException e) {
+			System.err.println("Failed to connect");
+		}
+		return connection;
+	}
+	
 }
