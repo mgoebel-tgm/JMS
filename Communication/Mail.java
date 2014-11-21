563e60be84cc8538604a40ea3ad70768948e2462
tperny-tgm <tperny@student.tgm.ac.at>
2014-11-21 14:56:26 +0100

    Mail Klasse begonnen zu implementieren

--- Communication/Mail.java
+++ Communication/Mail.java
@@ -1,16 +1,73 @@
 package Communication;
 
-import javax.jms.Connection;
+import java.util.Vector;
 
-public class Mail {
+import javax.jms.*;
+
+import Start.CheckArguments;
+
+/**
+ * @author Tobi
+ *
+ */
+public class Mail implements MessageListener{
 	private String user;
 	private Connection connection;
-	
+	private Session session;
+	private Destination destination;
+	private MessageConsumer consumer;
+	private Vector<String> m_emailMessage;
+
 	public Mail(String user, Connection connection){
 		this.user = user;
 		this.connection = connection;
+
 	}
 	public void startMailbox(){
-		
+		try {
+			session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+
+			destination = session.createQueue(CheckArguments.getIP());
+
+			consumer = session.createConsumer(destination);
+		} catch (JMSException e) {
+			System.err.println("hehehe you did something wrong");
+		}
+	}
+	public void readMails() {
+		try {
+			consumer.setMessageListener(this);
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			//keine neue Nachricht
+			if (m_emailMessage.size() == 0)
+				System.out.println("No new messages..");
+
+			//alle Nachrichten werden angezeigt
+			for (int i = 0; i < m_emailMessage.size(); i++)
+				System.out.println(m_emailMessage.elementAt(i));
+
+			m_emailMessage.clear();
+			consumer.setMessageListener(null);
+
+		} catch (JMSException e) {
+		}
+	}
+	@Override
+	public void onMessage(Message msg) {
+		try {
+			//Textnachricht generieren
+			TextMessage message = (TextMessage) msg;
+
+			//text wird zu vector hinzugefuegt
+			m_emailMessage.add(message.getText());
+
+		} catch (JMSException e) {
+		}
 	}
 }
